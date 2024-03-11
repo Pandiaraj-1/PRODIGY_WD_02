928d0f59c23216c4d9d57976e216195f444b2df5
Pandiaraj-1 <162461839+Pandiaraj-1@users.noreply.github.com>
2024-03-11 18:27:24 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,74 +1,53 @@
+let timer;
+let isRunning = false;
 let startTime;
-let running = false;
-let elapsedTime = 0;
-let interval;
+let lapCount = 1;
 
-function startStopwatch() {
-    if (!running) {
-        startTime = Date.now() - elapsedTime;
-        interval = setInterval(updateDisplay, 10);
-        running = true;
-        document.querySelector('.start').textContent = 'Stop';
+function startStop() {
+    if (isRunning) {
+        clearInterval(timer);
+        document.getElementById('startStop').innerText = 'Start';
+        isRunning = false;
     } else {
-        clearInterval(interval);
-        running = false;
-        document.querySelector('.start').textContent = 'Start';
-    }
-}
-
-function pauseStopwatch() {
-    clearInterval(interval);
-    running = false;
-    document.querySelector('.start').textContent = 'Start';
-}
-
-function resetStopwatch() {
-    clearInterval(interval);
-    running = false;
-    elapsedTime = 0;
-    updateDisplay();
-    document.querySelector('.start').textContent = 'Start';
-    document.querySelector('.laps').innerHTML = '';
-}
-
-function recordLap() {
-    if (running) {
-        const lapTime = Date.now() - startTime;
-        const formattedLapTime = formatTime(lapTime);
-        const lapItem = document.createElement('li');
-        lapItem.textContent = formattedLapTime;
-        document.querySelector('.laps').appendChild(lapItem);
+        startTime = Date.now() - (lapCount > 1 ? parseInt(document.getElementById('display').innerText.replace(/:/g, '')) * 10 : 0);
+        timer = setInterval(updateDisplay, 10);
+        document.getElementById('startStop').innerText = 'Stop';
+        isRunning = true;
     }
 }
 
 function updateDisplay() {
-    const currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
+    const elapsedTime = Date.now() - startTime;
     const formattedTime = formatTime(elapsedTime);
-    document.querySelector('.display').textContent = formattedTime;
+    document.getElementById('display').innerText = formattedTime;
 }
 
 function formatTime(time) {
-    const hours = Math.floor(time / (60 * 60 * 1000));
-    const minutes = Math.floor((time % (60 * 60 * 1000)) / (60 * 1000));
-    const seconds = Math.floor((time % (60 * 1000)) / 1000);
-    const milliseconds = time % 1000;
-    return (
-        pad(hours, 2) +
-        ':' +
-        pad(minutes, 2) +
-        ':' +
-        pad(seconds, 2) +
-        '.' +
-        pad(milliseconds, 3)
-    );
+    const hours = Math.floor(time / (1000 * 60 * 60));
+    const minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((time % (1000 * 60)) / 1000);
+    const milliseconds = Math.floor((time % 1000) / 10);
+
+    return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}.${pad(milliseconds)}`;
 }
 
-function pad(number, length) {
-    let str = String(number);
-    while (str.length < length) {
-        str = '0' + str;
-    }
-    return str;
+function pad(num) {
+    return num.toString().padStart(2, '0');
 }
 
+function reset() {
+    clearInterval(timer);
+    document.getElementById('display').innerText = '00:00:00';
+    document.getElementById('startStop').innerText = 'Start';
+    isRunning = false;
+    lapCount = 1;
+    document.getElementById('laps').innerHTML = '';
+}
+
+function lap() {
+    const lapTime = document.getElementById('display').innerText;
+    const lapItem = document.createElement('li');
+    lapItem.innerText = `Lap ${lapCount}: ${lapTime}`;
+    document.getElementById('laps').prepend(lapItem);
+    lapCount++;
+}
